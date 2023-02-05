2ee408f561f9690ebf627e14582e5ef16d398c6f
Diar <diarselimi92+github@gmail.com>
2023-02-05 16:58:36 +0100

    undo

--- components/Tag.js
+++ components/Tag.js
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 import kebabCase from '@/lib/utils/kebabCase'
 
-const Tag = ({ text }) => {
+const Tag = ({ text, count }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-sm font-medium uppercase">{text.split(' ').join('-')}</a>
+      <a className="mr-3 rounded-md bg-sky-500 p-5 text-sm font-medium uppercase text-white hover:bg-sky-500">
+        {text.split(' ').join('-')} {count}
+      </a>
     </Link>
   )
 }
