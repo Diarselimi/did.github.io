d5b1895c47fbd95e1e6930716658a01639dc6763
Diar <diarselimi92+github@gmail.com>
2023-02-05 15:09:41 +0100

    ttt

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,12 +15,12 @@ module.exports = {
   darkMode: 'media',
   theme: {
     extend: {
-      backgroundImage: (theme) => ({
+      backgroundImage: {
         'dark-hero':
           "url('https://diarselimi-github-io-ix9b-git-bundle-php-api-diarselimi.vercel.app/static/images/hero_dark.jpg')",
         'light-beams':
           "url('https://diarselimi-github-io-ix9b-git-bundle-php-api-diarselimi.vercel.app/static/images/light-beams.png')",
-      }),
+      },
       spacing: {
         '9/16': '56.25%',
       },
