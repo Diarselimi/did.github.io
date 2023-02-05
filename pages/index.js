e8545e76579c9cd8e023939edf4dfe3e92f37efd
Diar <diarselimi92+github@gmail.com>
2023-02-05 22:39:41 +0100

    Fix tags

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ export default function Home({ posts }) {
                           {path && (
                             <Link
                               href={`/tags/${path_tag}`}
-                              className="border-1 rounded-md border-primary-200 bg-primary-500 p-1 text-xs text-white"
+                              className="border-1 rounded border-primary-200 bg-primary-500 p-1 text-xs text-white"
                             >
                               Path: {path}
                             </Link>
