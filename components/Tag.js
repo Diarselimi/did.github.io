ccefc88de0755362b9a1b2c2bf9adb721f730b4b
Diar <diarselimi92+github@gmail.com>
2023-02-05 17:08:04 +0100

    last change

--- components/Tag.js
+++ components/Tag.js
@@ -4,9 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 rounded-md bg-sky-500 p-5 text-sm font-medium uppercase text-white hover:bg-sky-500">
-        {text.split(' ').join('-')}
-      </a>
+      <a className="mr-3 text-sm font-medium uppercase">{text.split(' ').join('-')}</a>
     </Link>
   )
 }
