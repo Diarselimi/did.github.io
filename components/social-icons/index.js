de60cbab1afc787fee245d29b0c98f94815949bb
did <diarselimi92@gmail.com>
2023-02-08 09:28:51 +0100

    fix icons

--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -8,63 +8,12 @@ import Twitter from './twitter.svg'
 // Icons taken from: https://simpleicons.org/
 
 const components = {
-  mail: Mail,
-  github: Github,
-  facebook: Facebook,
-  youtube: Youtube,
-  linkedin: Linkedin,
-  twitter: Twitter,
-}
-
-const svgs = {
-  linkedin: (
-    <svg
-      aria-hidden="true"
-      fill="currentColor"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 512 512"
-      className="h-6 w-6"
-    >
-      <title>Linkedin</title>
-      <path d="M136 183v283H42V183h94zm6-88c1 27-20 49-53 49-32 0-52-22-52-49 0-28 21-49 53-49s52 21 52 49zm333 208v163h-94V314c0-38-13-64-47-64-26 0-42 18-49 35-2 6-3 14-3 23v158h-94V183h94v41c12-20 34-48 85-48 62 0 108 41 108 127z"></path>
-    </svg>
-  ),
-  twitter: (
-    <svg
-      aria-hidden="true"
-      fill="currentColor"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 512 512"
-      className="h-6 w-6"
-    >
-      <title>Twitter</title>
-      <path d="m459 152 1 13c0 139-106 299-299 299-59 0-115-17-161-47a217 217 0 0 0 156-44c-47-1-85-31-98-72l19 1c10 0 19-1 28-3-48-10-84-52-84-103v-2c14 8 30 13 47 14A105 105 0 0 1 36 67c51 64 129 106 216 110-2-8-2-16-2-24a105 105 0 0 1 181-72c24-4 47-13 67-25-8 24-25 45-46 58 21-3 41-8 60-17-14 21-32 40-53 55z"></path>
-    </svg>
-  ),
-  facebook: (
-    <svg
-      aria-hidden="true"
-      fill="currentColor"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 512 512"
-      className="h-6 w-6"
-    >
-      <title>Facebook</title>
-      <path d="M379 22v75h-44c-36 0-42 17-42 41v54h84l-12 85h-72v217h-88V277h-72v-85h72v-62c0-72 45-112 109-112 31 0 58 3 65 4z"></path>
-    </svg>
-  ),
-  github: (
-    <svg
-      aria-hidden="true"
-      fill="currentColor"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 512 512"
-      className="h-6 w-6"
-    >
-      <title>Github</title>
-      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-    </svg>
-  ),
+    mail: Mail,
+    github: Github,
+    facebook: Facebook,
+    youtube: Youtube,
+    linkedin: Linkedin,
+    twitter: Twitter,
 }
 
 const SocialIcon = ({ kind, href, size = 8 }) => {
@@ -75,12 +24,12 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
 
   return (
     <a
-      className="ease mb-1 mr-1 inline-flex items-center rounded-full border-2 border-blue-600 p-3 text-white transition duration-200 hover:border-blue-700 hover:bg-blue-700"
+      className="ease mb-1 mr-1 inline-flex items-center rounded-full border-2 border-primary-600 p-3 text-black dark:text-white transition duration-200 hover:border-primary-700 hover:text-white hover:bg-primary-700"
       target="_blank"
       rel="noopener noreferrer"
       href={href}
     >
-      {svgs[kind]}
+      {SocialSvg}
     </a>
   )
 }
