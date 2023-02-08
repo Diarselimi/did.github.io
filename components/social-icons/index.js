547a129a3e67a200f444a4226d12b271db2445fa
did <diarselimi92@gmail.com>
2023-02-08 09:39:23 +0100

    fix

--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -24,12 +24,12 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
 
   return (
     <a
-      className="ease mb-1 mr-1 inline-flex items-center rounded-full border-2 border-primary-600 p-3 text-primary transition duration-200 hover:border-primary-700 hover:text-white hover:bg-primary-700"
+      className="text-primary·mb-1·mr-1·inline-flex·items-center·rounded-full·border-2·border-primary-600·p-3·transition·duration-200·hover:border-primary-700·hover:bg-primary-700·hover:text-white"
       target="_blank"
       rel="noopener noreferrer"
       href={href}
     >
-      {SocialSvg}
+        {SocialSvg}
     </a>
   )
 }
