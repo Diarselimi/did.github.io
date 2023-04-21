dcb6fc404f8fa4a89fddba7ab886abe11fb5884b
Diar <diarselimi92+github@gmail.com>
2023-04-22 00:58:38 +0200

    fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,10 @@
 import '@/css/tailwind.css'
-import '@/css/prism.css'
-import 'katex/dist/katex.css'
-
-import '@fontsource/inter/variable-full.css'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
-import { ClientReload } from '@/components/ClientReload'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
@@ -21,8 +15,6 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
-      {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
